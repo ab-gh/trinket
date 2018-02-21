494cbb20c86d5ec7c2ffc03b688e862ecd1194bc
electric-blue-green <thomas.bass2001@gmail.com>
2018-02-21 21:29:43 +0000

    tidy, and command line login

--- rgbval.py
+++ rgbval.py
@@ -1,10 +1,8 @@
 import board
 import busio
 import time
- 
 dotstar = busio.SPI(board.APA102_SCK, board.APA102_MOSI)
-print("INIT")
-
+print("INIT") # REPL
 def setPixel(red, green, blue):
     if not dotstar.try_lock():
         return
@@ -15,6 +13,6 @@ def setPixel(red, green, blue):
     dotstar.write(data)
     dotstar.unlock()
     time.sleep(0.01)
-
 while True:
     setPixel(*(int(c) for c in input("r, g, b: ").split(", ")))
+
