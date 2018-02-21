b6893a572337b41f13b1df3d550ffe6d2634bf58
Thomas Bass <thomas.bass2001@gmail.com>
2018-02-21 21:20:52 +0000

    rgbval

---
+++ rgbval.py
@@ -0,0 +1,20 @@
+import board
+import busio
+import time
+ 
+dotstar = busio.SPI(board.APA102_SCK, board.APA102_MOSI)
+print("INIT")
+
+def setPixel(red, green, blue):
+    if not dotstar.try_lock():
+        return
+    print("setting pixel to: %d %d %d" % (red, green, blue))
+    data = bytearray([0x00, 0x00, 0x00, 0x00,
+                      0xff, blue, green, red,
+                      0xff, 0xff, 0xff, 0xff])
+    dotstar.write(data)
+    dotstar.unlock()
+    time.sleep(0.01)
+
+while True:
+    setPixel(*(int(c) for c in input("r, g, b: ").split(", ")))
