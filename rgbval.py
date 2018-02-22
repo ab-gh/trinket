3bdc1b1f7522be126aa96263aec53221d2208614
electric-blue-green <thomas.bass2001@gmail.com>
2018-02-22 08:24:58 +0000

    smaller still

--- rgbval.py
+++ rgbval.py
@@ -7,10 +7,7 @@ def setPixel(red, green, blue):
     if not dotstar.try_lock():
         return
     print("setting pixel to: %d %d %d" % (red, green, blue))
-    data = bytearray([0x00, 0x00, 0x00, 0x00,
-                      0xff, blue, green, red,
-                      0xff, 0xff, 0xff, 0xff])
-    dotstar.write(data)
+    dotstar.write(bytearray([0x00, 0x00, 0x00, 0x00, 0xff, blue, green, red, 0xff, 0xff, 0xff, 0xff]))
     dotstar.unlock()
     time.sleep(0.01)
 while True:
