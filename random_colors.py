910982496e3867796b0bac00557c8781efcd7d38
electric-blue-green <thomas.bass2001@gmail.com>
2018-02-23 09:47:39 +0000

    random colors (with lots of help from @mattipv4)

---
+++ random_colors.py
@@ -0,0 +1,32 @@
+import board
+import busio
+import time
+import random
+dotstar = busio.SPI(board.APA102_SCK, board.APA102_MOSI)
+#colors = [1, 128, 244]      # set colors all to 1
+colors = [random.randint(3, 240), random.randint(3, 240), random.randint(3, 240), ]     # selects random start color in "safe zone"
+steps = [1, 3, 4]       # set wavelength
+steps = [random.randint(1, 5), random.randint(1, 5), random.randint(1, 5)]              # selects random step beteween 1 and 5
+print("INIT") ## REPL
+def getColor(index, colors, steps):
+    if colors[index] >= 255 or colors[index] <= 0:      # flip the sign of the step at the max/min
+        steps[index] *= -1
+    colors[index] += steps[index]                       # increment the value
+    if colors[index] > 255: colors[index] = 255         # accounting for stepping over 255
+    if colors[index] < 0: colors[index] = 0             # accounting for stepping under 0
+    return (colors[index], colors, steps)               # returns colors for index
+    
+def setPixel(red, green, blue):                         # call setpixel
+    if not dotstar.try_lock():                          # see if clock is locked
+        return
+    #print("setting pixel to: %d %d %d" % (red, green, blue))   # debug
+    dotstar.write(bytearray([0x00, 0x00, 0x00, 0x00, 0xff, blue, green, red, 0xff, 0xff, 0xff, 0xff]))
+    dotstar.unlock()                                    # pass new color
+
+while True:
+    r, colors, steps = getColor(0, colors, steps)       # gets red
+    g, colors, steps = getColor(1, colors, steps)       # gets green
+    b, colors, steps = getColor(2, colors, steps)       # gets blue
+    print("STEP = ", steps, "COLOR = ", colors)         # REPL debug print
+    setPixel(r, g, b)                                   # calls setPixel
+    time.sleep(random.random())                         # random wait time between 0 and 1
